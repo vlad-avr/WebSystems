83117fcfabbc5f4721214569065a29a0544ec541
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-09-24 14:50:04 +0300

    Implemented simple server

---
+++ src/main/java/vlad/avr/lab1/controller/ProductController.java
@@ -0,0 +1,26 @@
+package vlad.avr.lab1.controller;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products/{productId}")
+    public Product getProduct(@PathVariable int productId) {
+        return new Product(productId, productId + " name");
+    }
+
+    @Data
+    public static class Product {
+        private int id;
+        private String name;
+
+        public Product(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}

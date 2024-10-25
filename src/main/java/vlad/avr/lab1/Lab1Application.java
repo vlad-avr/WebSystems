baafca43d5c3b1041471afe0115af4cd03e2ef40
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2024-10-25 22:54:44 +0300

    Integrated Grafana and Prometheus (sort of)

--- src/main/java/vlad/avr/lab1/Lab1Application.java
+++ src/main/java/vlad/avr/lab1/Lab1Application.java
@@ -1,13 +1,26 @@
 package vlad.avr.lab1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Lab1Application {
+
+	final static Logger logger = LoggerFactory.getLogger(Lab1Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1Application.class, args);
 	}
 
+	@GetMapping("/something")
+	public ResponseEntity<String> createLogs() {
+		logger.warn("Just checking");
+		return ResponseEntity.ok().body("All Ok");
+	}
 }
